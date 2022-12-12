0505fcd4d4b344b4d7b703a49f37e57b9ca1c971
Tilaman7 <phxcommand@gmail.com>
2022-12-12 04:20:16 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Write proper grammer
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
