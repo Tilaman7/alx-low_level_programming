415294e3371e311276e42df4e0b33260a4da6528
Tilaman7 <phxcommand@gmail.com>
2022-12-12 15:01:58 -0800

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase, followed by a new line
+ *
+ * Return :Always 0
+*/
+int main(void)
+{
+	int digit1, digit2;
