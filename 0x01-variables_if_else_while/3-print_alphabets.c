42b0133ced06fb71d3818f0cdef32c79fea9fdbb
Tilaman7 <phxcommand@gmail.com>
2022-12-12 16:06:23 -0800

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,12 @@
 
 int main(void)
 {
-	char c;
-	char d;
+	char letter;
 
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
-		}
-	while
-		(d <= 'z') {
-			putchar(d);
-			d++;
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 }
